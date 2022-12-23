966f5327c09d040cdac7552df19c3e25dd137bf1
Morozan Andrei <andreimorozantiberiu@gmail.com>
2022-12-23 23:31:12 +0200

    ADD - CRUD Operations

--- src/app/services/crud.service.ts
+++ src/app/services/crud.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+export const URL =  'http://localhost/topnft-server/';
+
+
+const token   =  window.localStorage.getItem('token');
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
 
-  constructor() { }
+  heather = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+
+  token   =  localStorage.getItem('token');
+
+  constructor(private http: HttpClient) { }
+
+  getAll(url:any) {
+    return this.http.get(URL+url, {headers: this.heather});
+  }
+  
+  get(url:any) {
+    return this.http.get(URL+url, {headers: this.heather});
+  }
+
+  post(url:any, data:any) {
+    return this.http.post(URL+url, data , {headers: this.heather});
+  }
+
+  update(url:any, data:any) {
+    return this.http.post(URL+url, data, {headers: this.heather});
+  }
+
+  delete(url:any) {
+    return this.http.delete(URL+url, {headers: this.heather});
+  } 
+  deleteAll(url:any) {
+    return this.http.delete(URL+url, {headers: this.heather});
+  } 
+
+  //JWT Token
+  getToken(): String {
+    return window.localStorage['token'];
+  }
+
+  saveToken(token: String) {
+    window.localStorage['token'] = token;
+  }
+
+  destroyToken() {
+    window.localStorage.removeItem('token');
+  }
+  
 }
